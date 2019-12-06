aba58caf31475225364d4b0114ea0d650cb7bdcb
Dacy Tomasz <tomasz.dacy@sapiens.com>
2019-12-06 14:50:49 +0100

    After 9 - BrowserDriverFactory

--- BEGINNING/advanced-selenium-webdriver/src/main/java/com/herokuapp/theinternet/base/BaseTest.java
+++ BEGINNING/advanced-selenium-webdriver/src/main/java/com/herokuapp/theinternet/base/BaseTest.java
@@ -1,8 +1,6 @@
 package com.herokuapp.theinternet.base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -11,29 +9,11 @@ import org.testng.annotations.Parameters;
 public class BaseTest extends TestUtilities {
     protected WebDriver driver;
 
-    @Parameters({ "browser" })
+    @Parameters({"browser"})
     @BeforeMethod
     public void setUp(@Optional("chrome") String browser) {
-        // Create driver
-        System.out.println("Create driver: " + browser);
-
-        switch (browser) {
-        case "chrome":
-            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-            driver = new ChromeDriver();
-            break;
-
-        case "firefox":
-            System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-            driver = new FirefoxDriver();
-            break;
-
-        default:
-            System.out.println("Do not know how to start: " + browser + ", starting chrome.");
-            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-            driver = new ChromeDriver();
-            break;
-        }
+        BrowserDriverFactory factory = new BrowserDriverFactory(browser);
+        driver = factory.createDriver();
         driver.manage().window().maximize();
     }
 
